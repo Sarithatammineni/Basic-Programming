be1740a92b10a7a9d865c67f4b0c16b1f09afb03
Sarithatammineni <sarithatammineni18@gmail.com>
2025-11-20 23:09:13 +0530

    Add files via upload

---
+++ templateliterals.js
@@ -0,0 +1,9 @@
+let username='user';
+let course='JavaScript';
+console.log(`Hello ${username},welcome to the ${course}!`);
+
+const name="Sam";
+const age=21;
+const student={name,age,greet:()=>console.log("Hello")};
+
+const getFullName=(first,last)=>`${first} ${last}`;
