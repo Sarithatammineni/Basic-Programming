a42c35a193fb46a406d4b20a16243652fa6457b1
Sarithatammineni <sarithatammineni18@gmail.com>
2025-11-20 23:20:41 +0530

    Add files via upload

--- spreadtest.js
+++ spreadtest.js
@@ -2,3 +2,15 @@ arr1=[1,2,3];
 arr2=[4,5];
 let mergedArray=[...arr1,...arr2];
 console.log(mergedArray);
+
+function sum(...nums){
+    return nums.reduce((acc,num)=>acc + num,0);
+}
+console.log(sum(1,2,3,4));
+
+
+const user={
+    name:"Alice",age:22,address:{city :"Banglore",pin:560001}
+};
+const {name,address:{city,pin}}=user;
+console.log(name,city,pin);
